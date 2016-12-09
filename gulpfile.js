7f07f0b24ec78e9fe19f4ba50918198c587029e1
zhi.w <zhi@uqugu.com>
2016-12-09 09:02:25 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    browserSync = require('browser-sync');
+
+gulp.task('pug', function(){
+    return gulp.src('pug/*.pug')
+    .pipe(pug({
+        pretty: true
+    }).on('error', function(){}))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('sass', function(){
+    return gulp.src('scss/*.scss')
+    .pipe(sass({
+
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+
+reload = browserSync.reload;
+gulp.task('default', function(){
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    gulp.watch(['pug/*.pug'], ['pug']).on('change', reload);
+    gulp.watch(['scss/*.scss'], ['sass']).on('change', reload);
+});
