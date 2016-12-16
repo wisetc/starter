5fbba01b5048c2d433495b73a3eb6e3d85007508
zhi.w <zhi@uqugu.com>
2016-12-16 18:10:50 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,45 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     browserSync = require('browser-sync').create();
 
+var files = {
+    pug: 'pug/*.pug',
+    markup: ['pug/*.pug', 'pug/**/*.html'],
+    styles: 'scss/*.scss',
+    scripts: 'js/*.js'
+}
+reload = browserSync.reload;
+
 gulp.task('pug', function(){
-    return gulp.src('pug/*.pug')
+    return gulp.src(files.pug)
     .pipe(pug({
         pretty: true
-    }).on('error', function(){}))
+    }).on('error', function(){
+        console.log('pug went wrong.');
+        this.end();
+    }))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('sass', function(){
-    return gulp.src('scss/*.scss')
+    return gulp.src(files.styles)
     .pipe(sass({
 
     }).on('error', sass.logError))
     .pipe(gulp.dest('css'));
 });
 
-reload = browserSync.reload;
-gulp.task('default', function(){
+gulp.task('watch', function(){
+    gulp.watch(files.styles, ['sass', reload]);
+    gulp.watch(files.markup, ['pug', reload]);
+    return gulp.watch(files.scripts, reload);
+});
+
+gulp.task('serve', function(){
     browserSync.init({
         server: {
             baseDir: '.'
         }
     });
-    gulp.watch(['pug/*.pug'], ['pug']).on('change', reload);
-    gulp.watch(['scss/*.scss'], ['sass']).on('change', reload);
 });
+
+gulp.task('default', ['watch', 'pug', 'sass', 'serve']);
