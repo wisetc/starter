0b2d39bd648595c2aa4196c23a2b203b964937c5
zhi.w <zhi@uqugu.com>
2017-05-31 09:26:30 +0800

    gulpfile fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,30 @@
 var gulp = require('gulp'),
     scss = require('gulp-sass'),
     pug = require('gulp-pug'),
-    plumber = require('gulp-plumber'),
-    gutil = require('gulp-util'),
     notify = require('gulp-notify'),
     browserSync = require('browser-sync').create();
 
+var sourceFile = {
+    scss: 'scss/*.scss',
+    pug: 'pug/**/*.pug'
+}
+
+var outputDir = {
+    css: 'css/site',
+    html: '.'
+}
+
 gulp.task('scss', function(){
-    return gulp.src('scss/*/*.scss')
+    return gulp.src(sourceFile.scss)
         // .pipe(scss({outputStyle: 'compressed'}).on('error', scss.logError))
         .pipe(scss().on('error', scss.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest(outputDir.css));
 });
 
 gulp.task('pug', function(){
-    return gulp.src('pug/*/*.pug')
-        // .pipe(plumber())
+    return gulp.src(sourceFile.pug)
         .pipe( pug({ pretty: true }).on('error', notify.onError( (error) => { return `pug went wrong, ${error}`; } )) )
-        .pipe(gulp.dest('page'));
+        .pipe(gulp.dest(outputDir.html));
 });
 
 gulp.task('build', ['scss', 'pug']);
@@ -31,7 +38,7 @@ gulp.task('default', ['scss', 'pug'], function(){
             baseDir: '.'
         }
     });
-    gulp.watch('scss/*/*.scss', {cwd: cwd}, ['scss', browserSync.reload]);
-    gulp.watch('pug/*/*.pug', {cwd: cwd}, ['pug', browserSync.reload]);
+    gulp.watch(sourceFile.scss, {cwd: cwd}, ['scss', browserSync.reload]);
+    gulp.watch(sourceFile.pug, {cwd: cwd}, ['pug', browserSync.reload]);
 });
 
